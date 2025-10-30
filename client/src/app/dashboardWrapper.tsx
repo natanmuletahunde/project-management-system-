270221974af1133d46818279bfe12796fb7c57d4
pivnati <natanmuleta911@gmail.com>
2025-10-30 05:47:19 -1200

    first commit

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,28 @@
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from './(components)/Sidebar';
+import React from 'react';
+import StoreProvider from "./redux";
+
+const DashboardLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+         {/* sidebar*/}
+             <Sidebar/>
+        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+            {/* navbar */}
+            <Navbar/>
+            {children}
+        </main>
+    </div>
+  );
+}
+const DashboardWrapper =   ({children}:{children:React.ReactNode})=>{
+    return(
+        <StoreProvider>
+            <DashboardLayout>
+                {children}
+            </DashboardLayout>
+        </StoreProvider>
+    )
+}
+export default DashboardWrapper;
---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-col justify-center items-center min-h-screen bg-white'>
+   <h1 className='text-yellow-300'> </h1>
+    </div>
+  )
+}
+
+export default page
