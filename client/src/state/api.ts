d239ba707f08b2ae2d72c496249171e88c7039a3
pivnati <natanmuleta911@gmail.com>
2025-11-03 08:48:18 -1200

    modify  api files

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,16 +1,59 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getProjects } from '../../../server/src/contollers/projectController';
 export interface Project {
-    id:number;
+    id:number; //  this are mandatory
     name:string;
-    description?:string;
+    description?:string; // this three things are optional
     startDate?:string;
     endDate?:string;
 }
+export enum Priority {
+    Urgent = "Urgent",
+    High = "High",
+    Medium = "Medium",
+    Low = "Low",
+    Backlog = "Backlog",
+  }
+
+  export enum Status {
+    ToDo = "To Do",
+    WorkInProgress = "Work In Progress",
+    UnderReview = "Under Review",
+    Completed = "Completed",
+  }
+  export interface Task {
+    id: number;
+    title: string;
+    description?: string;
+    status?: Status;
+    priority?: Priority;
+    tags?: string;
+    startDate?: string;
+    dueDate?: string;
+    points?: number;
+    projectId: number;
+    authorUserId?: number;
+    assignedUserId?: number;
+  }
 
 export const api = createApi({
-    baseQuery:fetchBaseQuery({baseUrl:process.env.PUBLIC_API_BASE_URL}),
+    baseQuery :fetchBaseQuery({baseUrl:process.env.PUBLIC_API_BASE_URL}),
     reducerPath:"api",
-    tagTypes:[],
-    endpoints:(build)=>({})
+    tagTypes:["Projects"],
+    endpoints:(build)=>({
+        getProjects:build.query<Project[], void>({
+              query:()=> 'projects',
+              providesTags: [{ type: "Projects" }]
+        }),
+
+        createProject: build.mutation<Project, Partial<Project>>({
+            query: (project) => ({
+              url: "projects",
+              method: "POST",
+              body: project,
+            }),
+            invalidatesTags: ["Projects"],
+          }),
+    })
 })
 export const {}= api;
