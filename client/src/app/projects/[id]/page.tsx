247e5d99b54e9870a8231c301c411c211ca29211
pivnati <natanmuleta911@gmail.com>
2025-11-04 02:49:46 -1200

    create the project header in the project folder

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
+import ProjectHeader from "@/app/projects/[id]/ProjectHeader";
+type Props = {
+    params:{id:string}
+}
+const Project = ({params}: Props) => {
+    const {id} = params;
+    const [activeTab, setActiveTab] = useState("Board");
+    const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false)
 
-const  page = () => {
   return (
     <div>
-
+        {/*  new task*/}
+        <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab}/>
     </div>
   )
 }
-
-export default  page
+export default Project
