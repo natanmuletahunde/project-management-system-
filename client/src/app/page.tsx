054c6bdd7fe4e3732f727cb8c63e3282f5ddefd8
pivnati <natanmuleta911@gmail.com>
2025-11-06 01:25:00 -1200

    create the home  in the app folder

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import HomePage from './home/page'
 
 const page = () => {
   return (
-    <div className='flex flex-col justify-center items-center min-h-screen bg-white'>
-   <h1 className='text-yellow-300'> </h1>
-    </div>
+   <HomePage/>
   )
 }
 
