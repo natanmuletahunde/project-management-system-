42e11a177013b89869cdf0c9da3ce1367d48d5a9
pivnati <natanmuleta911@gmail.com>
2025-11-06 00:03:41 -1200

    create the search the file in the app folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import projectRoutes from "./routes/projectRoutes"
 import taskRoutes from "./routes/taskRoutes"
+import searchRoutes from "./routes/searchRoutes"
 
 //Routes Import
 // configurations
@@ -25,6 +26,7 @@ app.get('/', (req,res)=>{
 })
 app.use('/projects',projectRoutes)
 app.use('/tasks',taskRoutes)
+app.use('/search',searchRoutes)
 // server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
